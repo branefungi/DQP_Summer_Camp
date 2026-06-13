a7c72b077aad90f5e666db4cd9af6cdf23229e72
branefungi <benjaminzeng3@gmail.com>
2026-06-13 17:49:14 -0400

    Update Reference.java

--- Reference.java
+++ Reference.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
+@Disabled
 public class Reference extends LinearOpMode{
 
     @Override
