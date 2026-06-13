4343b454e3402892fe809b295d83d4da69361dd0
branefungi <benjaminzeng3@gmail.com>
2026-06-13 17:46:18 -0400

    Update MecanumDrivetrain.java

--- MecanumDrivetrain.java
+++ MecanumDrivetrain.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "Mecanum Drivetrain", group = "TeleOp")
