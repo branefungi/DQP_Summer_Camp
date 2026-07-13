e47d3530b9230cffb2deaddecc6454c50b9f133e
branefungi <benjaminzeng3@gmail.com>
2026-07-13 09:20:06 -0400

    Update SummerCamp.java

--- SummerCamp.java
+++ SummerCamp.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Teleop;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "Teleop", group = "Teleop")
 public class SummerCamp extends OpMode {
@@ -15,21 +16,28 @@ public class SummerCamp extends OpMode {
         drivetrain[1][0] = hardwareMap.get(DcMotor.class, "FR");
         drivetrain[0][1] = hardwareMap.get(DcMotor.class, "BL");
         drivetrain[1][1] = hardwareMap.get(DcMotor.class, "BR");
+
+        drivetrain[0][0].setDirection(DcMotor.Direction.REVERSE);
+        drivetrain[1][0].setDirection(DcMotor.Direction.FORWARD);
+        drivetrain[0][1].setDirection(DcMotor.Direction.REVERSE);
+        drivetrain[1][1].setDirection(DcMotor.Direction.FORWARD);
+
     }
+
 
     @Override
     public void loop(){
-        double y = -gamepad1.left_stick_y;
-        double x = -gamepad1.left_stick_x;
-        double rx = -gamepad1.right_stick_x;
-        double powerFL = (y + x) * rx;
-        double powerFR = (y - x) * -rx;
-        double powerBL = (y - x) * rx;
-        double powerBR = (y + x) * - rx;
+        double y = -gamepad1.left_stick_y * 0.8;
+        double x = gamepad1.left_stick_x * 0.8;
+        double rx = gamepad1.right_stick_x * 0.6;
+        double powerFL = y + x + rx;
+        double powerFR = y - x - rx;
+        double powerBL = y - x + rx;
+        double powerBR = y + x - rx;
         double max = Math.max(powerFL, powerFR);
         max = Math.max(max, powerBL);
         max = Math.max(max, powerBR);
-        if(max > 1.0){
+        if(max > 0.8){
             powerFL /= max;
             powerFR /= max;
             powerBL /= max;
